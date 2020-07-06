c2ab65f1f7541f3c90dac893a631439a81c1a8f5
arkiver <arkiver@protonmail.com>
2020-07-06 08:32:29 -0400

    Fix version 20200706.01.

--- pipeline.py
+++ pipeline.py
@@ -54,7 +54,7 @@ if not WGET_AT:
 #
 # Update this each time you make a non-cosmetic change.
 # It will be added to the WARC files and reported to the tracker.
-VERSION = '20200622.01'
+VERSION = '20200706.01'
 #with open('user-agents', 'r') as f:
 #    USER_AGENT = random.choice(f.read().splitlines()).strip()
 USER_AGENT = 'Archive Team 2'
